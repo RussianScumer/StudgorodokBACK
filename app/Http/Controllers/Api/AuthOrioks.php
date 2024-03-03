5361183e28f77a368663e0d4a18e0e41f0706ced
Korneevec Ilia <korneevecin@mail.ru>
2024-03-03 23:54:31 +0300

    fix

--- app/Http/Controllers/Api/AuthOrioks.php
+++ app/Http/Controllers/Api/AuthOrioks.php
@@ -14,6 +14,7 @@ class AuthOrioks extends Controller
          $token = new Tokens();
          $token->acctoken = $accToken;
          $token->save();
-         echo $token;
+         echo "token:";
+         echo $token->acctoken;
 }
 }
